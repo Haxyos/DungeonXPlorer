0775a1f6799ec5d022de3e36edc9d94e6f415714
AtsukaDev <atsukarisme@gmail.com>
2025-11-25 10:31:42 +0100

    (ADD) Ajout du footer

--- php/components/footer.php
+++ php/components/footer.php
@@ -1,9 +1,9 @@
-
-
-
-
 </body>
-<footer>
-    
+<footer class="fixed bottom-0 bg-[#941515] w-full h-32">
+
+    <h2 class="text-white text-center font-bold text-xl pt-4">DungeonXplorer</h2>
+    <h4 class="text-white text-center font-bold pt-6">Made with ❤️</h4>
+
 </footer>
+
 </html>
