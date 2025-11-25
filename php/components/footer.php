fcb633d4caf19302267e07da30ade8de194633d0
AtsukaDev <atsukarisme@gmail.com>
2025-11-25 10:40:09 +0100

    (EDIT) Modification du header et du footer

--- php/components/footer.php
+++ php/components/footer.php
@@ -1,9 +1,6 @@
 </body>
-<footer class="fixed bottom-0 bg-[#941515] w-full h-32">
-
+<footer class="bg-[#941515] w-full h-32 mt-auto">
     <h2 class="text-white text-center font-bold text-xl pt-4">DungeonXplorer</h2>
     <h4 class="text-white text-center font-bold pt-6">Made with ❤️</h4>
-
 </footer>
-
 </html>
