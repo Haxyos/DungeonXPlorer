a0f29dbef313484d74e9987b0970f689caaf88eb
Mary Ilhan <ilhanmary427@gmail.com>
2025-12-02 13:14:44 +0100

    Changement organisation pour la gestion de l'icône de profil

--- php/components/security_admin.php
+++ php/components/security_admin.php
@@ -4,10 +4,12 @@ include_once("../Database.php");
 $userId = $_SESSION['user_id'];
 if (!$userId) {
     header("Location: /index.php");
+    exit;
 }
 $stmt = $db->prepare('SELECT * FROM users WHERE id = :id');
 $stmt->execute(['id' => $userId]);
 $user = $stmt->fetch(PDO::FETCH_ASSOC);
 if (!$user['est_admin']) {
     header("Location: /index.php");
+    exit;
 }
