dc28072dd1d0babf411d79c3498bc5fc4dc14541
Haxyos <delacoudrew@gmail.com>
2025-12-02 13:21:04 +0100

    (UPDATE) update file CaracterCreation

--- View/CaracterCreation.php
+++ View/CaracterCreation.php
@@ -36,6 +36,12 @@ if (isset($_POST["characterName"]) && isset($_POST["class"]) && isset($_POST["de
             $stmt->execute(array('valeur1' => $stealer->getName(), 'valeur2' => $stealer->getClass(), 'valeur3' => $stealer->getImSrc(), 'valeur4' => $stealer->getBiography(), 'valeur5' => $stealer->getHealth(), 'valeur6' => $stealer->getMana(), 'valeur7' => $stealer->getStrength(), 'valeur8' => $stealer->getInitiative(), 'valeur9' => $stealer->getArmorItem(), 'valeur10' => $stealer->getPrimaryWeapon(), 'valeur12' => $stealer->getExp(), 'valeur13' => $stealer->getLevel(), 'valeur14' => $_SESSION['user_id']));
             break;
     }
+
+} else {
+    $warrior = new Warrior('Default', 0, null, 'je suis le personnage par default', 0, 0, 0, 0);
+    $db->prepare('INSERT INTO Hero (id, name, class_id, image, biographie, pv, mana, strength, initiative, armor_item_id, primary_weapon_item_id, shield_item_id, xp, current_level, user_id) VALUES (:valeur1, :valeur2, :valeur3, :valeur4, :valeur5, :valeur6, :valeur7, :valeur8, :valeur9, :valeur10, :valeur11, :valeur12 ,:valeur13, :valeur14)');
+    $stmt->execute(array('valeur1' => $warrior->getName(), 'valeur2' => $warrior->getClass(), 'valeur3' => $warrior->getImSrc(), 'valeur4' => $warrior->getBiography(), 'valeur5' => $warrior->getHealth(), 'valeur6' => $warrior->getMana(), 'valeur7' => $warrior->getStrength(), 'valeur8' => $warrior->getInitiative(), 'valeur9' => $warrior->getArmorItem(), 'valeur10' => $warrior->getPrimaryWeapon(), 'valeur11' => $warrior->getShield(), 'valeur12' => $warrior->getExp(), 'valeur13' => $warrior->getLevel(), 'valeur14' => $_SESSION['user_id']));
+
 }
 /*include "../php/components/header.php";*/
 ?>
@@ -83,6 +89,7 @@ if (isset($_POST["characterName"]) && isset($_POST["class"]) && isset($_POST["de
             <option></option>
             <option></option>
         </div>
+        <button type="submit">Personnage par default</button>
         <button type="submit">Construire mon personnage</button>
     </form>
 </body>
